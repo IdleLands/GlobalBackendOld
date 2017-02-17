98a83867687cf0273297eb19cff1f71f227aded0
seiyria <kyle@seiyria.com>
2017-02-17 09:09:53 -0600

    better error message

--- src/db.js
+++ src/db.js
@@ -2,7 +2,7 @@ const { MongoClient } = require('mongodb');
 
 const DB_URI = process.env.MONGODB_URI;
 if(!DB_URI) {
-  console.error('No env.DB_URI set. Set one.');
+  console.error('No env.MONGODB_URI set. Set one.');
   process.exit(0);
 }
 
